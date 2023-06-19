e48678c0b1b237aec4e12a4c36b879e53d8ecf0d
Hansol Hong <hh1834@nyu.edu>
2023-06-19 16:31:07 -0500

    Add second activity and assign random number on demand

---
+++ app/src/main/java/com/project/luckynumber/LuckyActivity.java
@@ -0,0 +1,56 @@
+package com.project.luckynumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class LuckyActivity extends AppCompatActivity {
+
+    TextView luckyNumberTextView;
+    Button shareBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lucky);
+
+        luckyNumberTextView = findViewById(R.id.luckyNumberTextView);
+        shareBtn = findViewById(R.id.shareBtn);
+
+        Intent i = getIntent();
+        String userName = i.getStringExtra("name");
+        Toast.makeText(this, "User Name: " + userName, Toast.LENGTH_LONG).show();
+
+        int randomNumber = generateRandomNumber();
+        luckyNumberTextView.setText(randomNumber + "");
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareData(userName, randomNumber);
+            }
+        });
+    }
+
+    private int generateRandomNumber() {
+        Random rand = new Random();
+        return rand.nextInt(1000);
+    }
+
+    private void shareData(String userName, int randomNumber) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+
+        i.putExtra(Intent.EXTRA_SUBJECT, userName);
+        i.putExtra(Intent.EXTRA_TEXT, randomNumber);
+
+        startActivity(Intent.createChooser(i, "Choose a platform"));
+    }
+}
--- app/src/main/java/com/project/luckynumber/MainActivity.java
+++ app/src/main/java/com/project/luckynumber/MainActivity.java
@@ -2,13 +2,32 @@ package com.project.luckynumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nameText;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameText = findViewById(R.id.nameEditText);
+        btn = findViewById(R.id.signUpBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = nameText.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), LuckyActivity.class);
+                intent.putExtra("name", userName);
+                startActivity(intent);
+            }
+        });
     }
 }
